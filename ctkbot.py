3d7afa2fdddb2b96c88eb8943877d2f362b49b27
vladrm <vlad_rym@mail.ru>
2022-02-03 23:45:01 +0300

    научились эмоджи и контексту юзверя

--- ctkbot.py
+++ ctkbot.py
@@ -1,28 +1,53 @@
+from glob import glob
 import logging
+from random import choice
+from emoji import emojize
+from telegram import ReplyKeyboardMarkup
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from config import token
+from config import token, USER_EMOJI
  
 
-logging.basicConfig(filename="bot.log", level=logging.INFO) 
+#logging.basicConfig(format="%(asctime)s - %(levelname)s - %(messages)s",
+#                    filename="bot.log",
+#                    level=logging.INFO) 
 
+
+def get_smile(user_data):
+    if "emoji" not in user_data:
+        smile = choice(USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data["emoji"]
 
 def greet_user(update, context):
     print("Ввызван /start")
-    update.message.reply_text("Здравствуй")
-    #print(update.message)
-    #update.message.reply_text(text)
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f"Здравствуй {context.user_data['emoji']}")
+    print(context.user_data)
+    
+    
  
 def talk_to_me(update, context):
-    text = update.message.text
-    print(text)
-    update.message.reply_text(text)
+    user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+        #logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username)
+    print(user_text)
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f" {user_text} {context.user_data['emoji']}")
  
+def send_cat_pictures(update, context):
+    cat_list = glob("images/*.jp*g")
+    cat_pic = choice(cat_list)
+    context.bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, "rb"))
+
 def main():
-    ctkbot=Updater(token,use_context=True)
+    ctkbot=Updater(token)
+
+    #logging.info("Бот запускается")
     
     dp=ctkbot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("cat", send_cat_pictures))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
  
     ctkbot.start_polling()
